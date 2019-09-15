ae011b6c644b6718d8a16df66857cde638835750
vishwa5555 <48879290+vishwa5555@users.noreply.github.com>
2019-09-15 21:51:51 +0530

    TABLE TOP ROBOT
    table top robot moves on the table without falling down onto the floor.The edges of the table are sensed using the IR sensors.

---
+++ table.c
@@ -0,0 +1,83 @@
+#include<avr/io.h>
+#include<util/delay.h>
+void main()
+{
+	DDRA=0b00000000;
+	DDRB=0b11111111;
+	PORTA=0b11111111;
+	int c;
+	while(1)
+	{
+		c=PINA;
+		if(c==0b11110000)
+			PORTB=0b00001010;
+		if(c==0b11111000)
+		{	PORTB=0b00000101;
+			_delay_ms(300);
+			PORTB=0b00001001;
+		}
+		if(c==0b11110100)
+		{
+			PORTB=0b00000101;
+			_delay_ms(300);
+			PORTB=0b00000110;
+			_delay_ms(200);
+		}
+		if(c==0b11110010)
+		{
+			PORTB=0b00001010;
+			_delay_ms(300);
+			PORTB=0b00001001;
+			_delay_ms(200);
+		}
+		if(c==0b11110001)
+		{
+			PORTB=0b00001010;
+			_delay_ms(300);
+			PORTB=0b00000110;
+			_delay_ms(200);
+		}
+		if(c==0b11110011)
+			PORTB=0b00001010;
+		if(c==0b11110110)
+		{
+			PORTB=0b00000110;
+			_delay_ms(300);
+			
+		}
+		if(c==0b11111100)
+		{
+			PORTB=0b00000101;
+			_delay_ms(300);
+			PORTB=0b00000110;
+			_delay_ms(200);
+		}
+		if(c==0b11111001)
+		{
+			PORTB=0b00001001;
+			_delay_ms(300);
+			
+		}
+		if(c==0b11110111)
+		{
+			PORTB=0b00000110;
+			_delay_ms(300);
+		}
+		if(c==0b11111011)
+		{
+			PORTB=0b00001001;
+			_delay_ms(300);
+		}
+		if(c==0b11111101)
+		{
+			PORTB=0b00000110;
+			_delay_ms(300);
+		}
+		if(c==0b11111110)
+		{
+			PORTB=0b00001001;
+			_delay_ms(300);
+		}
+	}
+}
+
