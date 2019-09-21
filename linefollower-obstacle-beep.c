66e6c656faf988f7b50374ba8708353ea0204ee6
vishwajith <48879290+vishwa-jith@users.noreply.github.com>
2019-09-21 12:11:46 +0530

    LINEFOLLOWER-OBSTACLE-BEEP
    A simple line follower robot which produces beep sound when the robot comes across a marker which is being sensed by the robot and it detects the obstacle in front of it with a beep sound.

--- linefollower-obstacle-beep.c
+++ linefollower-obstacle-beep.c
@@ -2,28 +2,28 @@
 #include<util/delay.h>
 void main()
 {
-	DDRA=0b00000000;//PART A is set as input port
-	DDRB=0b11111111;//PART B is set as output port
-	PORTA=0b11111111;//Pulling up port A
-	int c;//Declared to store the value of PIN A
+	DDRA=0b00000000;	//PART A is set as input port
+	DDRB=0b11111111;	//PART B is set as output port
+	PORTA=0b11111111;	//Pulling up port A
+	int c;		//Declared to store the value of PIN A
 	while(1)
 	{
-		c=PINA;//Value of PIN A is stored in c
-		if((c%8)/4==0)//Checking the value of PB2
+		c=PINA;		//Value of PIN A is stored in c
+		if((c%8)/4==0)		//Checking the value of PB2
 		{
-			PORTB=0b00010000;//Stop with beep sound
+			PORTB=0b00010000;	//Stop with beep sound
 		}
 		else
 		{
 			//Number of cases=4
-			if(c==0b11111100)//Both the sensors are glowing
-				PORTB=0b00001010;//Forward motion
-			if(c==0b11111111)//Both the sensors are not glowing
-				PORTB=0b00011010;//Forward motion with beep sound
-			if(c==0b11111101)//Left sensor will alone glow
-				PORTB=0b00001001;//Left wheel moves forward and right wheel moves backward
-			if(c==0b11111110)//Right sensor will alone glow
-				PORTB=0b00000110;//Right wheel moves forward and left wheel moves backward
+			if(c==0b11111100)	//Both the sensors are glowing
+				PORTB=0b00001010;	//Forward motion
+			if(c==0b11111111)	//Both the sensors are not glowing
+				PORTB=0b00011010;	//Forward motion with beep sound
+			if(c==0b11111101)	//Left sensor will alone glow
+				PORTB=0b00001001;	//Left wheel moves forward and right wheel moves backward
+			if(c==0b11111110)	//Right sensor will alone glow
+				PORTB=0b00000110;	//Right wheel moves forward and left wheel moves backward
 		}
 	}
 }
