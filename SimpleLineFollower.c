be600040dde46681b11985ed00fb163ab6896e5f
vishwajith <48879290+vishwa-jith@users.noreply.github.com>
2019-09-21 19:06:25 +0530

    SIMPLE LINE FOLLOWER
    The robot follows the black line by making use of the IR sensor which detects the surface and moves accordingly in the path.

--- SimpleLineFollower.c
+++ SimpleLineFollower.c
@@ -2,22 +2,21 @@
 #include<util/delay.h>	
 void main()
 {
-	DDRA=0b00000000;	//Input port assigned 0
-	DDRB=0b11111111;	//Output port
-	PORTA=0b11111111;	//Pulling up of Input port
-	int c;
+	DDRA=0b00000000;	//PART A is set as input port
+	DDRB=0b11111111;	//PART B is set as output port
+	PORTA=0b11111111;	//Pulling up port A
+	int c;		//Declared to store the value of PIN A
 	/* IR Sensor input pins PA[0],PA[1] */
 	while(1)
 	{
-		c=PINA;		//Reading the Port A input 
-		// Number of cases = 4
-		if(c==0b11111111) 	//Sensors -> 1,1
+		c=PINA;		//Value of PIN A is stored in c	 
+		if(c==0b11111111) 	//Both the sensors are not glowing
 			PORTB=0b00001010;	//Forward motion
-		if(c==0b11111110)	//Sensors -> 1,0
-			PORTB=0b00000110;	//Left turn
-		if(c==0b11111101)	//Sensors -> 0,1
-			PORTB=0b00001001;	//Right turn
-		if(c==0b11111100)	//Sensors -> 0,0
-			PORTB=0b00001010;	//Reverse
+		if(c==0b11111110)	//Right sensor will alone glow
+			PORTB=0b00000110;	//Right wheel moves forward and left wheel moves backward
+		if(c==0b11111101)	//Left sensor will alone glow
+			PORTB=0b00001001;	//Left wheel moves forward and right wheel moves backward
+		if(c==0b11111100)	//Both the sensors are glowing
+			PORTB=0b00001010;	//Forward motion
 	}
 }
