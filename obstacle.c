97cba59c61faa8949ed7f4630f2c397140bc209f
vishwajith <48879290+vishwa-jith@users.noreply.github.com>
2019-09-21 19:25:35 +0530

    OBSTACLE AVOIDER ROBOT
    The robot moves to  different direction based the 8 cases produced by the 3 IR sensors in front of the bot.

--- obstacle.c
+++ obstacle.c
@@ -2,56 +2,53 @@
 #include<util/delay.h>
 void main()
 {
-	DDRA=0b00000000;
-	DDRB=0b11111111;
-	PORTA=0b11111111;
-	PORTB=0b00000101;
-	_delay_ms(1000);
-	PORTB=0b00001001;
-	_delay_ms(1200);
-	int c;
+	DDRA=0b00000000;	//PART A is set as input port
+	DDRB=0b11111111;	//PART B is set as output port
+	PORTA=0b11111111;	//Pulling up port A
+	int c;		//Declared to store the value of PIN A
 	while(1)
-	{
-		c=PINA;
-		if(c==0b11111111)
-			PORTB=0b00001010;
-		if(c==0b11111110)
-			PORTB=0b00000110;
-		if(c==0b11111101)
+	{	
+		//Number of cases=8
+		c=PINA;		//Value of PIN A is stored in c
+		if(c==0b11111111)	//None of the sensors glows
+			PORTB=0b00001010;	//Forward motion
+		if(c==0b11111110)	//Third sensor alone glows
+			PORTB=0b00000110;	//Right wheel moves forward and left wheel moves backward
+		if(c==0b11111101)	//Second sensor alone glows
 		{
-			PORTB=0b00000000;
+			PORTB=0b00000000;	//Stops moving
 			_delay_ms(300);
-			PORTB=0b000010000;
+			PORTB=0b000010000;	//Produces a beep
 			_delay_ms(200);
 		}
-		if(c==0b11111100)
+		if(c==0b11111100)	//Second & Third sensors glows
 		{
-			PORTB=0b00000101;
+			PORTB=0b00000101;	//Reverse motion
 			_delay_ms(300);
-			PORTB=0b00000110;
+			PORTB=0b00000110;	//Right wheel moves forward and left wheel moves backward
 			_delay_ms(200);
 		}
-		if(c==0b11111011)
-			PORTB=0b00001001;
-		if(c==0b11111010)
+		if(c==0b11111011)	//First sensor alone glows
+			PORTB=0b00001001;	//Left wheel moves forward and right wheel moves backward
+		if(c==0b11111010)	//First & Third sensor glows
 		{
-			PORTB=0b00000101;
+			PORTB=0b00000101;	//Reverse motion
 			_delay_ms(300);
-			PORTB=0b00001001;
+			PORTB=0b00001001;	//Left wheel moves forward and right wheel moves backward
 			_delay_ms(200);
 		}
-		if(c==0b11111001)
+		if(c==0b11111001)	//First & Secoond sensor glows
 		{
-			PORTB=0b00000101;
+			PORTB=0b00000101;	//Reverse motion
 			_delay_ms(300);
-			PORTB=0b00001001;
+			PORTB=0b00001001;	//Left wheel moves forward and right wheel moves backward
 			_delay_ms(200);
 		}
-		if(c==0b11111000)
+		if(c==0b11111000)	//All the sensors are glowing
 		{
-			PORTB=0b00000101;
+			PORTB=0b00000101;	//Reverse motion
 			_delay_ms(1000);
-			PORTB=0b00001001;
+			PORTB=0b00001001;	//Left wheel moves forward and right wheel moves backward
 			_delay_ms(1200);
 		}
 	}
